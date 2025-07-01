09472c38839a36c167c5b0cbbfda3c98e863520b
susmithareddy-0 <susureddyvaddiboyina@gmail.com>
2025-07-01 21:52:34 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('send-text', (data) => {
+    socket.broadcast.emit('receive-text', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
